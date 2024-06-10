5fffc508c0b8d9ff3b0a26dc9e981a73f74fbfa8
patternplane <bss011202@naver.com>
2024-06-10 23:44:06 +0900

    [FEAT] 파일 열기 최대 갯수 제한 적용

--- ColorNote Backup Viewer/ViewModel/OpenFilesMenuViewModel.cs
+++ ColorNote Backup Viewer/ViewModel/OpenFilesMenuViewModel.cs
@@ -8,16 +8,19 @@ using System.Windows.Input;
 
 namespace ColorNote_Backup_Viewer.ViewModel
 {
-    public class OpenFilesMenuViewModel
+    public class OpenFilesMenuViewModel : ViewModelBase
     {
         public ICommand CNewFileOpen { get; }
         public MemoFilesData memoFilesData { get; private set; }
         private int _selectedIdx;
         public int selectedIdx { get { return _selectedIdx; } set { _selectedIdx = value; memoFilesData.select(selectedIdx); } }
+        public bool addButtonEnable { get; set; }
 
         public OpenFilesMenuViewModel(MemoFilesData memoFilesData)
         {
             this.memoFilesData = memoFilesData;
+            this.addButtonEnable = true;
+
             CNewFileOpen = new RelayCommand(obj => openFile());
         }
 
@@ -25,7 +28,15 @@ namespace ColorNote_Backup_Viewer.ViewModel
         {
             Model.BackupFileData newFile = NewWindowGenerator.ShowOepnNewFileDialog();
             if (newFile != null)
+            {
                 memoFilesData.addNewFile(newFile);
+                if (memoFilesData.openFilesList.Count >= 100)
+                {
+                    System.Windows.MessageBox.Show("백업 파일을 열 수 있는 최대 횟수 도달 : " + memoFilesData.openFilesList.Count + "/100)", "파일 수 최대", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                    addButtonEnable = false;
+                    NotifyPropertyChanged(nameof(addButtonEnable));
+                }
+            }
         }
     }
 }
